b045f1d97118ebb36659747b5c68659a18f936c0
Артем <im.aryunin@gmail.com>
2023-06-29 13:12:10 +0300

    AdeptRepository fixed

--- Server/src/main/java/com/shadowflex/Server/repository/AdeptRepository.java
+++ Server/src/main/java/com/shadowflex/Server/repository/AdeptRepository.java
@@ -3,5 +3,5 @@ package com.shadowflex.Server.repository;
 import com.shadowflex.Server.model.Adept;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdeptRepository extends JpaRepository<Long, Adept> {
+public interface AdeptRepository extends JpaRepository<Adept, Long> {
 }
